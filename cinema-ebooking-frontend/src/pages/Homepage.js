072bbffaa2b7e484c4770085431ef0ea2c024b93
Dev Patel <95192549+DevP25@users.noreply.github.com>
2024-02-20 16:13:24 -0500

    Hover added to carousel

--- cinema-ebooking-frontend/src/pages/Homepage.js
+++ cinema-ebooking-frontend/src/pages/Homepage.js
@@ -5,9 +5,12 @@ import logo_holder from '../components/logo_holder.jpg'
 import Carousel from '../components/Carousel';
 import axios from 'axios';
 import Button from '@mui/material/Button'
+import { createTheme } from '@mui/material/styles';
+import ReactPlayer from 'react-player'
 
 
 const Homepage = () => {
+    //Carousel
     const [items, setItems] = useState([]);
 
     useEffect(() => {
@@ -27,6 +30,18 @@ const Homepage = () => {
       });
     }, []);
 
+    //Color Pallette for button
+    const theme = createTheme({
+      palette: {
+        ochre: {
+          main: '#E3D026',
+          light: '#E9DB5D',
+          dark: '#A29415',
+          contrastText: '#242105',
+        },
+      },
+    });
+
     return(
         <>
             <nav>
@@ -42,9 +57,17 @@ const Homepage = () => {
                 <Button variant="contained">Login/Signup</Button>
             </nav>
 
-            <section>
-                <Carousel items={items}/>
+            <section className='playing_carousel'>
+              <h2 className='header_font'>Now Showing</h2>
+              <Carousel items={items}/>
             </section>
+
+            <section className='soon_carousel'>
+              <h2 className='header_font'>Coming Soon</h2>
+              <Carousel items={items}/>
+            </section>
+
+            
         </>
     )
 }
