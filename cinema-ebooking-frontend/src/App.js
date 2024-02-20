fe9e42d27d5fa2832397e8d2588500e7adcb694d
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2024-02-20 10:19:55 -0500

    AdminMainScreen routing

--- cinema-ebooking-frontend/src/App.js
+++ cinema-ebooking-frontend/src/App.js
@@ -3,39 +3,19 @@ import Carousel from './Carousel';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import Homepage from './components/Homepage'
+import AdminMainScreen from './AdminMainScreen';
 
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:3000/movies')
-      .then(response => {
-        const movies = response.data;
-        const mappedItems = movies.map(movie => {
-          return {
-            imageUrl: movie.thumbnailUrl,
-            title: movie.title
-          };
-        });
-        setItems(mappedItems);
-      })
-      .catch(error => {
-        console.error('Error fetching movies:', error);
-      });
-  }, []);
-
   return (
     <Router>
       <div>
         <Routes>
           <Route exact path='/' element={<Homepage />} />
+          <Route path='/AdminMainScreen' element={<AdminMainScreen />} />
         </Routes>
       </div>
     </Router>
-    
-    
-    
   );
 
 }
