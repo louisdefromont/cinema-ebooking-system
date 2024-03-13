56075e8cdcf3c98b28bc4b1064c226a2566a4689
Dev Patel <dev7712@gmail.com>
2024-03-13 18:00:55 -0400

    consolelogupdate

---
+++ cinema-ebooking-backend/loginfunc.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+const userRouter = express.Router();
--- cinema-ebooking-frontend/src/pages/Login.js
+++ cinema-ebooking-frontend/src/pages/Login.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 import './Login.css'
 import NavBar from '../components/NavBar';
 
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Email:", email); 	
+		console.log("Password:", password);
+		setEmail('');
+        setPassword('');
+    };
+
+	const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+	const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
 
     return(
         <>
             <NavBar />
 
 			<div class="form">
-				<form class="login-form">
-					<input type="text" placeholder="email"/>
-					<input type="password" placeholder="password"/>
+				<form class="login-form" onSubmit={handleSubmit}>
+					<input type="text" placeholder="email" value={email} onChange={handleEmailChange}/>
+					<input type="password" placeholder="password" value={password} onChange={handlePasswordChange}/>
 					<button className="ripple">login</button>
 					<p class="message">Not registered? <a href="/register">Create an account</a></p>
 				</form>
