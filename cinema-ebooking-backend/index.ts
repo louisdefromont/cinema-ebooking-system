3eed59868ab953b37c95fdc8c9c615fc11002c01
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2024-03-13 23:51:14 -0400

    remove body-parser

--- cinema-ebooking-backend/index.ts
+++ cinema-ebooking-backend/index.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
-import { BodyParser } from 'body-parser'
 import cors from 'cors'
 
 const prisma = new PrismaClient()
