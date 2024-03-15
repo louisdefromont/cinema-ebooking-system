ba51ffb752cc7ab687c413bc1c893c13e657dc08
elaine-philip <emp49872@uga.edu>
2024-03-15 17:42:34 -0400

    migration files

---
+++ cinema-ebooking-backend/prisma/migrations/20240315211054_unique_email/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `releaseDate` on the `Movie` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `dateTime` on the `Showing` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `Movie` MODIFY `releaseDate` DATETIME NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Showing` MODIFY `dateTime` DATETIME NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_email_key` ON `User`(`email`);
