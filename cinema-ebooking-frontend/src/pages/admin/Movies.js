0607fc88272dfef73297b45146b0f099c734f6c0
louisdefromont <88596228+louisdefromont@users.noreply.github.com>
2024-03-16 16:06:32 -0400

    admin movies get from db

--- cinema-ebooking-frontend/src/pages/admin/Movies.js
+++ cinema-ebooking-frontend/src/pages/admin/Movies.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TableManager from './TableManager';
+import Axios from 'axios';
 
 const Users = () => {
-    const [rows, setRows] = useState([{ "id": 1, "title": "Dune", "trailerUrl": "https://youtu.be/n9xhJrPXop4", "thumbnailUrl": "https://m.media-amazon.com/images/I/61QbqeCVm0L.jpg", "releaseDate": "2024-02-18T05:19:03.000Z" },
-    { "id": 2, "title": "Godzilla vs. Kong", "trailerUrl": "https://youtu.be/odM92ap8_c0", "thumbnailUrl": "https://m.media-amazon.com/images/M/MV5BZmYzMzU4NjctNDI0Mi00MGExLWI3ZDQtYzQzYThmYzc2ZmNjXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg", "releaseDate": "2024-02-18T05:19:03.000Z" },
-    { "id": 3, "title": "Kung Fu Panda 3", "trailerUrl": "https://youtu.be/10r9ozshGVE", "thumbnailUrl": "https://m.media-amazon.com/images/M/MV5BMTUyNzgxNjg2M15BMl5BanBnXkFtZTgwMTY1NDI1NjE@._V1_FMjpg_UX1000_.jpg", "releaseDate": "2024-02-18T05:19:03.000Z" },
-    { "id": 4, "title": "Deadpool", "trailerUrl": "https://youtu.be/ONHBaC-pfsk", "thumbnailUrl": "https://upload.wikimedia.org/wikipedia/en/2/23/Deadpool_%282016_poster%29.png", "releaseDate": "2024-02-18T05:19:03.000Z" }]);
+    const [rows, setRows] = useState([]);
+    useEffect(() => {
+        Axios.get('http://localhost:3000/movies').then((response) => {
+            setRows(response.data);
+        });
+    }, []);
 
     return (
         <TableManager rows={rows} setRows={setRows} rowHeaders={["id", "title", "trailerUrl", "thumbnailUrl", "releaseDate"]} />
