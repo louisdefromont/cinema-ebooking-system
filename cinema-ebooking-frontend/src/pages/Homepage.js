4f329713d07cc1e7feb6f7e7377101e29bba5c13
Dev Patel <95192549+DevP25@users.noreply.github.com>
2024-04-10 12:11:39 -0400

    Modal Component added to homepage and moviesearch

--- cinema-ebooking-frontend/src/pages/Homepage.js
+++ cinema-ebooking-frontend/src/pages/Homepage.js
@@ -90,54 +90,8 @@ const Homepage = () => {
         <h2 className='header_font'>Coming Soon</h2>
         <Carousel items={items} />
       </section>
-<<<<<<< Updated upstream
-      <Dialog open={selectedMovie !== null} onClose={() => setSelectedMovie(null)}>
-        <div className='display_top'>
-          <DialogTitle>{selectedMovie?.title}</DialogTitle>
-          <Button color="primary" onClick={() => setEditShowing(true)}> Edit </Button>
-        </div>
-        <DialogContent>
-          <iframe
-            width="560"
-            height="315"
-            src={"https://www.youtube.com/embed/" + selectedMovie?.trailerUrl.replace("https://youtu.be/", "")}
-            title={selectedMovie?.title}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <div>
-            <h3>{selectedMovie?.genres}</h3>
-            <span>{selectedMovie?.description}</span>
-            <br></br>
-            <br></br>
-            <h3>Showtimes:</h3>
-            <ul>
-              {selectedMovie?.showings.map(showing => (
-                <li key={showing.id}>
-                  <span>{new Date(showing.dateTime).toLocaleString()}</span>
-                  <Button onClick={() => handleSelectShowing(showing)} color="primary">
-                    Buy Tickets
-                  </Button>
-                </li>
-              ))}
-              <li>
-                <AddShowing movie={selectedMovie} />
-              </li>
-            </ul>
-          </div>
-          <EditShowing editShowing={editShowing} setEditShowing={setEditShowing} movie={selectedMovie} />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setSelectedMovie(null)} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-=======
 
       <Modal selectedMovie={selectedMovie} setSelectedMovie={setSelectedMovie} handleSelectShowing={handleSelectShowing} />
->>>>>>> Stashed changes
     </>
   )
 }
